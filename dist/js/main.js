7e585b6de7bba04c1032ce6141cfe9fb49aaa1ee
yankazimirchick <35384715+yankazimirchick@users.noreply.github.com>
2021-05-25 11:01:57 +0300

    update info

--- dist/js/main.js
+++ dist/js/main.js
@@ -51,17 +51,24 @@ $(function () {
     });
 
     // filter buttons
-    $('.filters-button-group').on('click', 'button', function () {
-        var filterValue = $(this).attr('data-filter');
-        $grid.isotope({
-            filter: filterValue
-        });
-    });
+    
+   
+
+    
     $('.button-group').each(function (i, buttonGroup) {
         var $buttonGroup = $(buttonGroup);
         $buttonGroup.on('click', 'button', function () {
             $buttonGroup.find('.is-checked').removeClass('is-checked');
             $(this).addClass('is-checked');
+        });
+    });
+    $grid.imagesLoaded().progress( function() {
+        $grid.isotope('layout');
+      });
+      $('.filters-button-group').on('click', 'button', function () {
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({
+            filter: filterValue
         });
     });
 });
